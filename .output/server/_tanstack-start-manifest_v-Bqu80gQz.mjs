b01f90d2f5b2f2e1abc782133f161dedfbf0a87f
Sourabh-prajapat25 <prajapatsourabh725@gmail.com>
2026-07-15 00:30:19 +0530

    second bug fix for Portfolio

---
+++ .output/server/_tanstack-start-manifest_v-Bqu80gQz.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-Bqu80gQz.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "D:/Projects/Portfolio/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-BRqlBHr9.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-BRqlBHr9.js"
+		} }]
+	},
+	"/": {
+		filePath: "D:/Projects/Portfolio/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-CR2g1nHd.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
